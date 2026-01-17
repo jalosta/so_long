7df726d5ad9132947a4dfafff906e8473e9cbbe0
jalosta- <jalosta-@student.42abudhabi.ae>
2026-01-17 16:58:40 +0400

    created header, Makefile, folders and README

---
+++ README.md
@@ -0,0 +1,7 @@
+*This project has been created as part of the 42 curriculum by jalosta-*
+
+# Description
+
+# Instructions
+
+# Resources
---
+++ includes/so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                           f                 */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalosta- <jalosta-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/17 15:03:28 by jalosta-          #+#    #+#             */
+/*   Updated: 2026/01/17 16:48:41 by jalosta-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+#include <unistd.h> // write, read
+#include <stdlib.h> // malloc, exit
+#include <fcntl.h> // open, close
+#include <math.h> // https://linux.die.net/man/3/math
+#include <string.h> // strerror
+#include <stdio.h> // perror
+
+#endif
